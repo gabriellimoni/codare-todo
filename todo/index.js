4a87bf065bbabd78c7fd58cb81cbd37cc3933fcf
gabrielblimoni <gabrielblimoni@gmail.com>
2020-09-12 18:53:10 -0300

    mode changing between localstorage and firebase

--- todo/index.js
+++ todo/index.js
@@ -1,15 +1,46 @@
 import FirebaseService from './src/services/firebase.js'
+import LocalStorageService from './src/services/localStorage.js'
 import TodoApp from './src/app.js'
 import InputHandler from './src/inputHandler.js'
 
-main()
-async function main () {
-    const firebaseService = new FirebaseService()
-    firebaseService.initializePersistencyService()
-    
-    const app = new TodoApp('TodoList', firebaseService)
-    await app.initializeTodoApp()
-    
-    const inputHandler = new InputHandler('TodoInput', app)
-    inputHandler.initializeInputHandler()
+const availablesPersistencyTypes = [
+    'localstorage',
+    'firebase',
+]
+
+export default class Main {
+    _persistencyType = ''
+    _persistencyService = null
+    _app = null
+
+    constructor (persistencyType='localstorage') {
+        if (!availablesPersistencyTypes.includes(persistencyType))
+            throw new Error(`Persistency type ${persistencyType} not available`)
+
+        this._persistencyType = persistencyType
+    }
+
+    async run () {
+        this._setPersistencyService()
+        
+        this._app = new TodoApp('TodoList', this._persistencyService)
+        await this._app.initializeTodoApp()
+        
+        const inputHandler = new InputHandler('TodoInput', this._app)
+        inputHandler.initializeInputHandler()
+    }
+
+    _setPersistencyService () {
+        switch (this._persistencyType) {
+            case 'localstorage': 
+                this._persistencyService = new LocalStorageService()
+                break
+            case 'firebase':
+                this._persistencyService = new FirebaseService()
+                break
+        }
+
+        if (!this._persistencyService) throw new Error('Cannot run without persistency service')
+        this._persistencyService.initializePersistencyService()
+    }
 }
---
+++ todo/src/services/localStorage.js
@@ -0,0 +1,36 @@
+export default class LocalStorageService {
+    initializePersistencyService () {}
+
+    async addTodo (todo) {
+        const allTodos = this._getAllTodosJSON()
+        allTodos.push(todo)
+
+        this._setAllTodos(allTodos)
+    }
+
+    async getAllTodos () {
+        const allTodos = this._getAllTodosJSON()
+        return allTodos
+    }
+
+    async removeTodoById (todoId) {
+        let allTodos = this._getAllTodosJSON()
+        const filteredTodos = allTodos.filter(todo => todo.id != todoId)
+        this._setAllTodos(filteredTodos)
+    }
+
+    _getAllTodosJSON () {
+        let allTodos = localStorage.getItem('todos')
+        if (!allTodos) {
+            localStorage.setItem('todos', JSON.stringify([]))
+            allTodos = localStorage.getItem('todos')
+        }
+
+        const allTodosJSON = JSON.parse(allTodos)
+        return allTodosJSON
+    }
+
+    _setAllTodos (todos) {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }
+}
